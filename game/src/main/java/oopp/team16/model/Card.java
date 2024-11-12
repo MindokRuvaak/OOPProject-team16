ab6bcacc4e885de002879315c3ab4b53a96795a3
kjellida <ida.kjellerstedt@hotmail.com>
2024-11-12 15:51:42 +0100

    simple card class

---
+++ game/src/main/java/oopp/team16/model/Card.java
@@ -0,0 +1,22 @@
+package oopp.team16.model;
+
+public class Card {
+
+    private final Color color;
+    private final Value value;
+
+    public Card(Color color, Value value){
+        this.color = color;
+        this.value = value;
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public Value getValue(){
+        return this. value;
+    }
+
+
+}
