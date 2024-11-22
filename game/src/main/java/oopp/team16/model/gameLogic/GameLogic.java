ce2eb0bf230b8ab2c1d9fbb7ff630839e390f064
MindokRuvaak <cole_daniel@rocketmail.com>
2024-11-22 16:16:18 +0100

    changed canPlay gamelogic method to usse boolean logic rather than if else and only take two cards rather than card and stack

--- game/src/main/java/oopp/team16/model/gameLogic/GameLogic.java
+++ game/src/main/java/oopp/team16/model/gameLogic/GameLogic.java
@@ -1,36 +1,20 @@
 package oopp.team16.model.gameLogic;
 
-import java.util.Stack;
-
 public class GameLogic {
-    boolean canPlay(Card card, Stack<Card> playedCard) {
-        //stack, peek
-        Card topCard = playedCard.peek();
 
-        if(card.getColor().equals(topCard.getColor())) {
-            return true;
-        }else
-            return card.getValue() == topCard.getValue(); // change when changing from enum
+    public boolean canPlay(Card card, Card topCard) {
+        return (card.getValue() == topCard.getValue()) || card.getColor().equals(topCard.getColor()); 
+        // change when changing from enum
+    }
+
+    // byta håll från reverse
+    void changeOrder() {
 
     }
 
-    //byta håll från reverse
-    void changeOrder(){
+    // byta spelare, måste wrap nånting
+    void nextPlayer() {
 
     }
-
-    //byta spelare, måste wrap nånting
-    void nextPlayer(){
-
-    }
-
-
-
-
-
-
-
-
-
 
 }
