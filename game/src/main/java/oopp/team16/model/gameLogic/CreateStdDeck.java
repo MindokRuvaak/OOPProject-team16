4b77bdfe639abce46b49a092b07031dd876997fa
kjellida <ida.kjellerstedt@hotmail.com>
2024-11-28 15:19:56 +0100

    changed in CreateStdDeck to use CardFactory

--- game/src/main/java/oopp/team16/model/gameLogic/CreateStdDeck.java
+++ game/src/main/java/oopp/team16/model/gameLogic/CreateStdDeck.java
@@ -2,14 +2,13 @@ package oopp.team16.model.gameLogic;
 
 import java.util.Stack;
 
-import oopp.team16.model.gameLogic.Cards.Card;
-import oopp.team16.model.gameLogic.Cards.BaseCard;
+import oopp.team16.model.gameLogic.Cards.*;
 import oopp.team16.model.gameLogic.Cards.Colors.StdColors;
-import oopp.team16.model.gameLogic.Cards.CardType;
 import oopp.team16.model.gameLogic.Cards.Colors.Color;
-import oopp.team16.model.gameLogic.Cards.NumCard;
 
 public class CreateStdDeck implements DeckFactory {
+
+    CardFactory cardFactory;
 
     @Override
     public Deck createDeck() {
@@ -19,9 +18,9 @@ public class CreateStdDeck implements DeckFactory {
 
         for (Color color : colors) {
             for (int i = 0; i <= 9; i++) {
-                stack.push(new NumCard(color, i)); // Single 0 per color , use to getValue() instead?
+                stack.push(cardFactory.createNumCard(color, i)); // Single 0 per color , use to getValue() instead?
                 if (i != 0) { // Two copies for 1–9 cards
-                    stack.push(new NumCard(color, i));
+                    stack.push(cardFactory.createNumCard(color, i));
                 }
 
             }
