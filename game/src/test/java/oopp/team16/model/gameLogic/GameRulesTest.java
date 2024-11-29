ab9c4cc0c4d0b57b19cda95b758aa187941e9e3e
MindokRuvaak <cole_daniel@rocketmail.com>
2024-11-29 14:43:10 +0100

    created a test file for GameRules

---
+++ game/src/test/java/oopp/team16/model/gameLogic/GameRulesTest.java
@@ -0,0 +1,15 @@
+package oopp.team16.model.gameLogic;
+
+import org.junit.jupiter.api.Test;
+
+public class GameRulesTest {
+    @Test
+    void testAllowedPlay() {
+
+    }
+
+    @Test
+    void testStackable() {
+
+    }
+}
