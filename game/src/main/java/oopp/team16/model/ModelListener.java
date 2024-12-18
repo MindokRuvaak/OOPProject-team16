9be25364d3e006042573e383f9c096358c8678c7
MindokRuvaak <cole_daniel@rocketmail.com>
2024-12-18 16:04:11 +0100

    removed commented code

--- game/src/main/java/oopp/team16/model/ModelListener.java
+++ game/src/main/java/oopp/team16/model/ModelListener.java
@@ -1,22 +1,11 @@
 package oopp.team16.model;
 
-// import oopp.team16.model.gameLogic.Cards.Card;
-
 public interface ModelListener {
 
-    // void update();
 
     void requestPlayers(int lower, int upper);
 
-    // void takeTurn();
-
-    // void announceBadMove();
-
     void announceWinner(String name, int score);
-
-    // void startNextPlayerTurn();
-
-    // void announceMustPlayCard();
 
     void requestWildColor();
 
