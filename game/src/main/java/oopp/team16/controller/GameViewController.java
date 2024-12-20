2b2956bfd127421700a5f83ea158db8d9a25468f
felixtan <felix3123@hotmail.com>
2024-12-20 15:37:45 +0100

    fixat view fxml

--- game/src/main/java/oopp/team16/controller/GameViewController.java
+++ game/src/main/java/oopp/team16/controller/GameViewController.java
@@ -61,6 +61,19 @@ public class GameViewController /* implements GameListener, ModelListener */ {
     private TextArea rulesText;
     @FXML
     private Label labelUno;
+    @FXML
+    private Button buttonBlue;
+    @FXML
+    private Button buttonYellow;
+    @FXML
+    private Button buttonGreen;
+    @FXML
+    private Button buttonRed;
+    @FXML
+    private AnchorPane paneColour;
+    @FXML
+    private Label labelBadMove;
+
 
     private final Map<Integer, Pane> playersHand = new HashMap<>();
     private final List<Pane> paneList = new ArrayList<>();
@@ -82,6 +95,7 @@ public class GameViewController /* implements GameListener, ModelListener */ {
         winningPane.setVisible(false);
         buttonDisplayHand.setVisible(false);
         rulesPane.setVisible(false);
+        paneColour.setVisible(false);
         loadGameRules();
         cc = new CreateCardView();
         paneList.add(player1Hand);
@@ -118,6 +132,18 @@ public class GameViewController /* implements GameListener, ModelListener */ {
         });
         buttonUno.setOnAction(event -> {
             uno();
+        });
+        buttonBlue.setOnAction(event -> {
+            paneColour.setVisible(false);
+        });
+        buttonGreen.setOnAction(event -> {
+            paneColour.setVisible(false);
+        });
+        buttonYellow.setOnAction(event -> {
+            paneColour.setVisible(false);
+        });
+        buttonRed.setOnAction(event -> {
+            paneColour.setVisible(false);
         });
     }
 
@@ -300,8 +326,7 @@ public class GameViewController /* implements GameListener, ModelListener */ {
 
     // @Override
     public void badMove() {
-        // TODO: implement gui
-        System.out.println("cant play that card");
+        labelBadMove.setText("cant play that card");
     }
 
     // @Override
@@ -314,9 +339,9 @@ public class GameViewController /* implements GameListener, ModelListener */ {
     public void requestWildColor() {
         // temporary termianl input
         // TODO: implement gui
+        paneColour.setVisible(true);
         // need winidow popup with 4 buttons, one for each color
-        // button "COLOR" calls setWildColor("COLOR") with "COLOR" to be the actual
-        // color name as string eg. "red"
+        //button "COLOR" calls setWildColor("COLOR") with "COLOR" to be the actual color name as string eg. "red"
     }
 
     public void addClientController(GameClientController gcc) {
